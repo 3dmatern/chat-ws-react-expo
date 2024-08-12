b6a4ac42740935e763d5f9c68b9831fe1a9c2acd
Denis Matern <denismatern@gmail.com>
2024-08-12 17:33:21 +0300

    feature: form, components

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
+
+import FormContainer from "@/components/form/FormContainer";
+import TextInputField from "@/components/form/TextInputField";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <FormContainer
+        title="Авторизация"
+        footer={{
+          text: "Хотели бы увидеть код?",
+          url: "https://github.com/3dmatern/chat-nuxt",
+          urlText: "GitHub"
+        }}
+      >
+        <TextInputField />
+      </FormContainer>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
