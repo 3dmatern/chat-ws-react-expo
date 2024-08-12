981d9a15d8b64937afa6b284fa821092571a26b2
Denis Matern <denismatern@gmail.com>
2024-08-12 16:04:58 +0300

    fix: config

---
+++ app.config.ts
@@ -0,0 +1,7 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+    ...config,
+    name: "Chat WS React Expo",
+    slug: "chat-ws-react-expo",
+});
